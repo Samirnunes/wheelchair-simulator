b1d965473b05ad27d29937ddf15ac2e95460791f
Samir Nunes da Silva <samir.silva12342@gmail.com>
2023-10-01 20:05:48 -0300

    add movement

--- src/index.js
+++ src/index.js
@@ -19,14 +19,6 @@ const light = new THREE.DirectionalLight(0xffffff, 1)
 light.position.set(2,2,5)
 scene.add(light)
 
-/* const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({
-    color: 'green'
-})
-const boxMesh = new THREE.Mesh(geometry, material)
-scene.add(boxMesh) */
-
-// Boiler Plate Code
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -35,6 +27,30 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100)
 camera.position.set(0, 1, 2)
 scene.add(camera)
+
+const cameraPosition = new THREE.Vector3(0, 1, 2);
+const cameraSpeed = 0.1;
+
+const arrowKeys = { left: false, right: false, up: false, down: false };
+
+document.addEventListener("mousemove", (event) => {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  });
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft") arrowKeys.left = true;
+  if (event.key === "ArrowRight") arrowKeys.right = true;
+  if (event.key === "ArrowUp") arrowKeys.up = true;
+  if (event.key === "ArrowDown") arrowKeys.down = true;
+});
+
+document.addEventListener("keyup", (event) => {
+  if (event.key === "ArrowLeft") arrowKeys.left = false;
+  if (event.key === "ArrowRight") arrowKeys.right = false;
+  if (event.key === "ArrowUp") arrowKeys.up = false;
+  if (event.key === "ArrowDown") arrowKeys.down = false;
+});
 
 const renderer = new THREE.WebGL1Renderer({
     canvas: canvas
@@ -46,9 +62,19 @@ renderer.shadowMap.enabled = true
 renderer.gammaOutput = true
 renderer.render(scene, camera)
 
-function animate(){
-    requestAnimationFrame(animate)
-    renderer.render(scene, camera)
-}
+function animate() {
+    requestAnimationFrame(animate);
+  
+    // Update camera position based on arrow key presses
+    if (arrowKeys.left) cameraPosition.x -= cameraSpeed;
+    if (arrowKeys.right) cameraPosition.x += cameraSpeed;
+    if (arrowKeys.up) cameraPosition.z -= cameraSpeed;
+    if (arrowKeys.down) cameraPosition.z += cameraSpeed;
+  
+    // Set the camera's new position
+    camera.position.copy(cameraPosition);
+  
+    renderer.render(scene, camera);
+  }
 
 animate()
