0732c764fe8711766203417b22df880394542325
Samir Nunes da Silva <samir.silva12342@gmail.com>
2023-10-01 19:53:26 -0300

    rendered scene succesfully

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import * as THREE from "../node_modules/three/build/three.module.js"
+import { GLTFLoader } from "../node_modules/three/examples/jsm/loaders/GLTFLoader.js"
+
+const canvas = document.querySelector('.webgl')
+const scene = new THREE.Scene()
+
+const loader = new GLTFLoader()
+loader.load("../assets/city.glb", function(glb){
+    console.log(glb)
+    const root = glb.scene;
+    scene.add(root);
+}, function(xhr){
+    console.log((xhr.loaded/xhr.total * 100) + "% loaded")
+}, function(error){
+    console.log("An error occurred")
+})
+
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set(2,2,5)
+scene.add(light)
+
+/* const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({
+    color: 'green'
+})
+const boxMesh = new THREE.Mesh(geometry, material)
+scene.add(boxMesh) */
+
+// Boiler Plate Code
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100)
+camera.position.set(0, 1, 2)
+scene.add(camera)
+
+const renderer = new THREE.WebGL1Renderer({
+    canvas: canvas
+})
+
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.shadowMap.enabled = true
+renderer.gammaOutput = true
+renderer.render(scene, camera)
+
+function animate(){
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+}
+
+animate()
